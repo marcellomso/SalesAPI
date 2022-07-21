9eb9f6badc1c99579ada30a2551e73fc98a28e40
Marcelo Santos <marcellosan@gmail.com>
2022-07-21 12:53:56 -0300

    Alterar nome método de teste de itens

--- Sales.Tests/ItemTest.cs
+++ Sales.Tests/ItemTest.cs
@@ -5,7 +5,7 @@ namespace Sales.Tests
     public class ItemTest
     {
         [Test]
-        public void ValidarDescricaoPreenchida()
+        public void ValidarProdutoExiste()
         {
             var item = new Item(0, null, 10);
             Assert.That(item.IsValid, Is.False);
